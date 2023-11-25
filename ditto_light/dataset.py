5e29a50fce3ffc76f61a056a17a209c71ca9f428
hoanghuydo787 <hoanghuydo787@gmail.com>
2023-11-25 20:43:40 +0700

    upd

--- ditto_light/dataset.py
+++ ditto_light/dataset.py
@@ -77,7 +77,9 @@ class DittoDataset(data.Dataset):
         # augment if da is set
         if self.da is not None:
             combined = self.augmenter.augment_sent(left + ' [SEP] ' + right, self.da)
-            left, right = combined.split(' [SEP] ')[0], combined.split(' [SEP] ')[1] 
+            print(combined.split(' [SEP] '))
+            print(type(combined.split(' [SEP] ')))
+            # left, right = combined.split(' [SEP] ') 
             x_aug = self.tokenizer.encode(text=left,
                                       text_pair=right,
                                       max_length=self.max_len,
